706b7b94a663e0aebecfc39c1f09ba02701eac18
xingxing.wang <xingxing.wang@live.com>
2016-12-18 20:18:30 +0800

    modify readme.md

--- server/src/test/java/com/tqmall/search/benz/ConfigTest.java
+++ server/src/test/java/com/tqmall/search/benz/ConfigTest.java
@@ -6,6 +6,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -19,9 +20,14 @@ public class ConfigTest {
     @Test
     public void yamlParseTest() {
         Path configPath = NlpUtils.getPathOfClass(ConfigTest.class, "/config.yml");
-        Settings settings = Settings.builder()
-                .loadFromPath(configPath)
-                .build();
+        Settings settings = null;
+        try {
+            settings = Settings.builder()
+                    .loadFromPath(configPath)
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         List<Config.AnalysisConfig> list = Config.AnalysisConfig.parse(settings);
         Assert.assertEquals(4, list.size());
         for (SegmentConfig s : list) {
